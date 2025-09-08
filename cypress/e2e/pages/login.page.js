78f676705406aed7ce92b0ee47e4e49631280583
bingmoreno2016 <bingmoreno2016@gmail.com>
2025-09-08 22:46:11 +0800

    login POM

--- cypress/e2e/pages/login.page.js
+++ cypress/e2e/pages/login.page.js
@@ -1,7 +1,9 @@
-export default class LoginPage {
+export default class loginPage {
     getLocators = {
     getFieldUsername: () => cy.get('#ctl00_MainContent_username'),
     getFieldPassword: () => cy.get('#ctl00_MainContent_password'),
+
+    getButtonLogin: () => cy.get('#ctl00_MainContent_login_button')
     }
 }
     
