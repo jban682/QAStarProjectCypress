ad591a6fbfeece191f9f8784f185337b7c169726
jban682 <jeanbanks682@gmail.com>
2025-09-15 08:54:09 +0800

    update the beforeEach to visit order page

--- cypress.config.js
+++ cypress.config.js
@@ -9,11 +9,14 @@ module.exports = defineConfig({
     BASE_URL: process.env.BASE_URL,
     USERNAME: process.env.USER,
     PASSWORD: process.env.PASSWORD,
+    ORDER_URL: process.env.ORDER_URL,
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      require('@cypress/grep/src/plugin')(config);
+      return config;
     },
   },
 });
--- cypress/e2e/tests/createOrder.cy.js
+++ cypress/e2e/tests/createOrder.cy.js
@@ -13,13 +13,15 @@ describe('OrderPage', () => {
         cy.visit(Cypress.env('BASE_URL'));
         cy.login(Cypress.env('USERNAME'), Cypress.env('PASSWORD'));
         
-        cy.get('a[href="Process.aspx"]').click();
-        cy.url().should('include', '/Process.aspx');
+        cy.reload();
+        cy.visit(Cypress.env('ORDER_URL'));
+        //cy.get('a[href="Process.aspx"]').click();
+        //cy.url().should('include', '/Process.aspx');
     }),
 
         it.only('[TC001] Verify Order page initial status', () => {
             cy.section('[TC001] Verify Order page initial status');
-            cy.get('h2').should('contain', 'Order');
+            cy.get('h2').should('exist').should('contain', 'Order');
 
             // cy.contains('h2', 'Order').should('be.visible');
             cy.contains('h3', 'Product Information').should('be.visible');
